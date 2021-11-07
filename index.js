9742048286ef385b9cf43bde438278da0b94bc0c
ronco1812 <89574940+ronco1812@users.noreply.github.com>
2021-11-07 15:43:25 +0200

    step4

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ app.get('/api/info',(req,res) =>{
             res.status(400).send('no such contact').end()
         }
       })
+
+      app.delete('/api/persons/:id', (req, res) => {
+        const id = Number(req.params.id)
+        persons = persons.filter(person => person.id !== id)
+      
+        res.status(204).end()
+      })
   
   const PORT = 3001
   app.listen(PORT, () => {
