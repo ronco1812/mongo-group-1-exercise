71af75dc5e0e2fd03359e862559b2045f8cdca60
ronco1812 <89574940+ronco1812@users.noreply.github.com>
2021-11-13 18:38:58 +0200

    again

--- front/front.js
+++ front/front.js
@@ -1,4 +1,4 @@
-const server = 'http://localhost:3001/';
+const server = '/';
 const body = document.getElementById("root");  
 const phoneBook = document.getElementById("phone-book");
 const getContacts = async () => {
